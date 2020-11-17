f20a064d9e1014e31d284e8c99955775e277e3a7
Calvin <calvinmorett@users.noreply.github.com>
2020-11-17 03:34:51 -0500

    testing async js
    dynamically load the xp values

---
+++ js/async_test.js
@@ -0,0 +1,65 @@
+const charname = document.createElement('h1')
+const charText = document.createTextNode('Character Name: Calvinhc');
+charname.appendChild(charText);
+document.body.appendChild(charname);
+charname.classList.add('player');
+
+const elem = document.createElement('li')
+// const elemText = document.createTextNode('346');
+document.body.appendChild(elem);
+elem.classList.add('child');
+
+var list = document.getElementsByClassName("xplog")[0];
+list.getElementsByClassName("child")[0].style.width = "54" + "px";
+
+const elem1 = document.createElement('li')
+document.body.appendChild(elem1);
+elem1.classList.add('child');
+list.getElementsByClassName("child")[1].style.width = "60" + "px";
+
+const elem2 = document.createElement('li')
+document.body.appendChild(elem2);
+elem2.classList.add('child');
+list.getElementsByClassName("child")[2].style.width = "48" + "px";
+
+const elem3 = document.createElement('li')
+document.body.appendChild(elem3);
+elem3.classList.add('child');
+list.getElementsByClassName("child")[3].style.width = "250" + "px";
+
+const elem4 = document.createElement('li')
+document.body.appendChild(elem4);
+elem4.classList.add('child');
+list.getElementsByClassName("child")[4].style.width = "170" + "px";
+
+const elem5 = document.createElement('li')
+document.body.appendChild(elem5);
+elem5.classList.add('child');
+list.getElementsByClassName("child")[5].style.width = "52" + "px";
+
+const elem6 = document.createElement('li')
+document.body.appendChild(elem6);
+elem6.classList.add('child');
+list.getElementsByClassName("child")[6].style.width = "68" + "px";
+
+
+// list.getElementsByClassName("child")[7].style.width = "39" + "px";
+// list.getElementsByClassName("child")[8].style.width = "150" + "px";
+// list.getElementsByClassName("child")[9].style.width = "770" + "px";
+// list.getElementsByClassName("child")[10].style.width = "122" + "px";
+// list.getElementsByClassName("child")[11].style.width = "238" + "px";
+// list.getElementsByClassName("child")[12].style.width = "99" + "px";
+// list.getElementsByClassName("child")[13].style.width = "150" + "px";
+// list.getElementsByClassName("child")[14].style.width = "570" + "px";
+// list.getElementsByClassName("child")[15].style.width = "32" + "px";
+// list.getElementsByClassName("child")[16].style.width = "58" + "px";
+// list.getElementsByClassName("child")[17].style.width = "99" + "px";
+
+// 48
+// 60
+// 48
+// 250
+// 170
+// 52
+// 68
+// 39
